9b047f5aad05f1d05f69fc9799be1323090c089e
Shanmukh459 <saikrrish459@gmail.com>
2023-11-20 17:39:19 -0800

    functionally working but need to change UI

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,8 @@ import { useState } from "react";
 import { ValidTaskStatesEnum } from "@/utils/task";
 import { BarsLoaderSpinner } from "./loader-spinners";
 import { robotoMono, montserrat } from "./fonts";
+import { ChatOpenAI } from "langchain/chat_models/openai"
+import { PromptTemplate } from "langchain/prompts"
 
 import classes from "./page.module.css";
 import VideoMetadata from "./components/VideoMetadata";
@@ -13,9 +15,19 @@ export default function Home() {
   const [videoUrl, setVideoUrl] = useState("");
   const [taskStatus, setTaskStatus] = useState(ValidTaskStatesEnum.INITIAL);
   const [transcriptionDocsList, setTranscriptionDocsList] = useState(undefined);
-  const [isTranscribeButtonDisabled, setIsTranscribeButtonDisabled] =
-    useState(false);
+  const [isTranscribeButtonDisabled, setIsTranscribeButtonDisabled] = useState(false);
+  const [output, setOutput] = useState('sample output')
 
+  const openAIApiKey = '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'
+  const llm = new ChatOpenAI({ openAIApiKey })
+
+  const queryTemplate = 'Summarize this yoube video subtitle: {subtitle}'
+
+  const queryPrompt = PromptTemplate.fromTemplate(queryTemplate)
+
+  const queryChain = queryPrompt.pipe(llm)
+
+  console.log("Output: " + output)
   function onInputChange(changeEvent) {
     if (changeEvent.target.name === "video-url") {
       setVideoUrl(changeEvent.target.value);
@@ -51,6 +63,10 @@ export default function Home() {
     setTranscriptionDocsList(transcribedDocs);
     setTaskStatus(ValidTaskStatesEnum.FINISHED);
     setIsTranscribeButtonDisabled(false); // re-enable transcribe button
+    const response = await queryChain.invoke({subtitle: transcribedDocs['0'].pageContent})
+    setOutput(response.content)
+    console.log("output:" + output)
+    // console.log(transcribedDocs['0'].pageContent)
   }
 
   return (
@@ -77,6 +93,12 @@ export default function Home() {
         </button>
       </form>
       {taskStatus === ValidTaskStatesEnum.LOADING && <BarsLoaderSpinner />}
+      <textarea
+        value={output}
+        name="output"
+        readOnly
+        >
+      </textarea>
       {transcriptionDocsList && (
         <>
           <hr className={classes.dottedHr} />
@@ -86,7 +108,7 @@ export default function Home() {
 
               return (
                 <li key={idx}>
-                  <VideoMetadata videoMetadataObj={docMetadata ?? {}} />
+                  {/* <VideoMetadata videoMetadataObj={docMetadata ?? {}} /> */}
                   <div className={classes.videoDetailsContainer}>
                     <h2
                       className={`${classes.pageSubHeader} ${robotoMono.className}`}
